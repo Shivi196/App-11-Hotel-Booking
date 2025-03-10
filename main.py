6f0aaa6374ab9baf02b56f1f44a0b6c743f5d0fa
misha <misha07.sharma@gmail.com>
2025-03-10 23:21:51 +0530

    Initial Commit: Code the Instances

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
 
 class Hotels:
-    def book(self):
+    def __init__(self,id):
+        pass
+
+    def book(self,id):
+        pass
+
+    def available(self):
         pass
 
 class ReservationTicket:
+    def __init__(self,customer_name,hotel_object):
+        pass
      def generate(self):
          pass
 
+print(df)
 
+id = input("Enter the id of hotel you wanna book: ")
+
+hotel = Hotels(id)
+if hotel.available():
+    hotel.book()
+    name = "Enter your name"
+    reservation_ticket = ReservationTicket(name,hotel)
+    reservation_ticket.generate()
+else:
+    print("Sorry,Hotel is not available/free!!!!")
