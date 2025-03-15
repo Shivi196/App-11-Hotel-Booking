ba7335d0eb5e71e426cbed9f29e4b3b264326339
misha <misha07.sharma@gmail.com>
2025-03-15 14:07:44 +0530

    3rd Commit: Implement the methods of Hotel & Reservation class

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv",dtype={"id":str})
 
 class Hotels:
-    def __init__(self,id):
-        pass
 
-    def book(self,id):
-        pass
+    def __init__(self,hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"]==self.hotel_id,"name"].squeeze()
 
     def available(self):
-        pass
+        """ Check if the hotel is available """
+        availability = df.loc[df["id"]== self.hotel_id,"available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+    def book(self):
+        """Book a hotel by changing its availability to no """
+        availability = df.loc[df["id"]== self.hotel_id,"available"] ="no"
+        df.to_csv(path_or_buf="hotels.csv",index=False)
+
+
 
 class ReservationTicket:
-    def __init__(self,customer_name,hotel_object):
-        pass
+
+     def __init__(self,customer_name,hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
      def generate(self):
-         pass
+         content = f"""
+         Thank you for your reservation!!
+         Here are your booking data:
+         Name : {self.customer_name}
+         Hotel Name : {self.hotel.name}
+         """
+         return content
 
 print(df)
 
-id = input("Enter the id of hotel you wanna book: ")
+hotel_ID = input("Enter the id of hotel you wanna book: ")
+hotel = Hotels(hotel_ID)
 
-hotel = Hotels(id)
 if hotel.available():
     hotel.book()
-    name = "Enter your name"
-    reservation_ticket = ReservationTicket(name,hotel)
-    reservation_ticket.generate()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(customer_name=name,hotel_object=hotel)
+    print(reservation_ticket.generate())
 else:
     print("Sorry,Hotel is not available/free!!!!")
